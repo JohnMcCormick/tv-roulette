2f83314970f07cac9319f35573462d5f49270589
John McCormick <johnmccormick91@gmail.com>
2023-12-03 12:27:24 +0000

    Added client to cors origins

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const express = require("express");
 const app = express();
+const cors = require('cors');
+
 const port = process.env.PORT || 3001;
+
+var corsOptions = {
+  origin: ['http://localhost:5173', 'https://tv-roulette.onrender.com'],
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
+app.use(cors(corsOptions))
 
 app.get("/api", (req, res) => {
   res.json({ "shows": ["The Sopranos", "Mad Men", "Friends"]})
