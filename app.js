04af0a79ea51550f8f96ea8ac3f00f0d2931a0ff
John McCormick <johnmccormick91@gmail.com>
2023-12-04 16:01:11 +0000

    Working with hardcoded bearer token

--- app.js
+++ app.js
@@ -1,8 +1,42 @@
 const express = require("express");
 const app = express();
 const cors = require('cors');
+require('dotenv').config()
 
 const port = process.env.PORT || 3001;
+
+const TVDBApiKey = process.env.API_KEY;
+const TVDBUrl = 'https://api4.thetvdb.com/v4/';
+console.log(TVDBApiKey)
+
+let { token: bearerToken } = require('./data/tvdb-bearer-token.json');
+
+const getBearerToken = async () => {
+  console.log('Fetching new bearer token')
+  let response = await fetch(`${TVDBUrl}login`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ "apikey": TVDBApiKey, "pin": null })
+  })
+  let { status, data } = await response.json()
+  let { token } = data;
+  console.log(token);
+  bearerToken = token;
+}
+
+const searchShow = async (show) => {
+  let response = await fetch(`${TVDBUrl}search?query=${show}&limit=5`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": `Bearer ${bearerToken}`,
+    },
+  })
+  let { status, data } = await response.json()
+  return data;
+}
 
 var corsOptions = {
   origin: ['http://localhost:5173', 'https://tv-roulette.onrender.com'],
@@ -12,10 +46,79 @@ var corsOptions = {
 app.use(cors(corsOptions))
 
 app.get("/api", (req, res) => {
-  res.json({ "shows": ["The Sopranos", "Mad Men", "Friends"]})
+  res.json({ "shows": [{"name": "The Sopranos", "tvdb_id": 75299}, {"name": "Mad Men", "tvdb_id": 80337}, {"name": "Friends", "tvdb_id": 79168}] })
 })
 
-const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.get("/search", async (req, res) => {
+  let show = req.query.show;
+  let response = await fetch(`${TVDBUrl}search?query=${show}&limit=5`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": `Bearer ${bearerToken}`,
+    },
+  })
+  let { status, data } = await response.json()
+  console.log(status, data);
+  res.json({ "shows": data })
+})
+
+const getExtendedSeriesData = async (showId) => {
+  let response = await fetch(`${TVDBUrl}series/${showId}/extended?meta=translations`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": `Bearer ${bearerToken}`,
+    },
+  })
+  let { status, data } = await response.json()
+  return { status, data }
+}
+
+const getDefaultSeasons = async (showId) => {
+  let { status, data } = await getExtendedSeriesData(showId);
+  let { defaultSeasonType, seasons } = data;
+  let defaultSeasons = seasons.filter(season => season.type.id === defaultSeasonType).filter(season => season.number > 0);
+  // console.log(defaultSeasons);
+
+  return defaultSeasons;
+}
+
+app.get("/select", async (req, res) => {
+  let showId = req.query.id;
+  console.log(`Show id: ${showId}`)
+  
+  let defaultSeasons = await getDefaultSeasons(showId);
+  
+  let numSeasons = defaultSeasons.length;
+  console.log(`Number of seasons ${numSeasons}`)
+  let seasonIndex = Math.floor(Math.random() * numSeasons);
+
+  let seasonId = defaultSeasons[seasonIndex].id;
+
+  let response = await fetch(`${TVDBUrl}seasons/${seasonId}/extended`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": `Bearer ${bearerToken}`,
+    },
+  });
+  let { status, data } = await response.json()
+
+  let { episodes } = data;
+  let numEpisodes = episodes.length;
+  let episodeIndex = Math.floor(Math.random() * numEpisodes)
+  res.json({ "seasonNumber": seasonIndex + 1, "episodeNumber": episodeIndex + 1, "data": episodes[episodeIndex] })
+})
+
+const server = app.listen(port, async () => {
+  console.log(`Example app listening on port ${port}!`)
+
+  if (bearerToken) console.log(bearerToken)
+  else {
+    await getBearerToken();
+  }
+});
 
 server.keepAliveTimeout = 120 * 1000;
 server.headersTimeout = 120 * 1000;
